Fixed home_range grid calculation to new discretization scheme.

home_range.py
@@ -5,25 +5,24 @@ import matplotlib.pyplot as plt
 from multiprocessing import Pool
 
 import movement
-from constants import *
+from constants import (ALPHA, BETA, DT, RANGE)
 
 PARAMETERS = {
     'alpha': ALPHA,
     'beta': BETA,
-    'dx': DX,
     'dt': DT,
     'range': RANGE
 }
 
+
 @contextmanager
-def set_constants(alpha=ALPHA, range=RANGE, dx=DX, dt=DT, beta=BETA):
-    global PARAMETERS 
+def set_constants(alpha=ALPHA, range=RANGE, dt=DT, beta=BETA):
+    global PARAMETERS
     tmp_constants = PARAMETERS
     PARAMETERS['alpha'] = alpha
-    PARAMETERS['beta'] = beta 
-    PARAMETERS['dx'] = dx 
-    PARAMETERS['dt'] = dt 
-    PARAMETERS['range'] = range 
+    PARAMETERS['beta'] = beta
+    PARAMETERS['dt'] = dt
+    PARAMETERS['range'] = range
     yield
     PARAMETERS = tmp_constants
 
@@ -31,13 +30,14 @@ def set_constants(alpha=ALPHA, range=RANGE, dx=DX, dt=DT, beta=BETA):
 def make_grid(home_range, constants=PARAMETERS):
 
     dt = PARAMETERS['dt']
-    dx = PARAMETERS['dx']
     alpha = PARAMETERS['alpha']
     range = PARAMETERS['range']
     beta = PARAMETERS['beta']
 
     velocity = movement.home_range_to_velocity(home_range, beta=beta, dt=dt)
-    mov_data = movement.make_data(velocity, num=1, steps=dt, alpha=alpha, range=range)
+    dx = max(velocity, 0.01)
+    mov_data = movement.make_data(
+        velocity, num=1, steps=dt, alpha=alpha, range=range)
     mov = mov_data['data'][0]
 
     shape = int(np.ceil(range/float(dx)))
@@ -47,24 +47,26 @@ def make_grid(home_range, constants=PARAMETERS):
     for index in indices:
         trace[index[0], index[1]] = 1
     return trace
-   
+
 
 def calculate(mobility, constants=PARAMETERS):
     dx = PARAMETERS['dx']
-    grid = make_grid(mobility, constants) 
-    area = np.sum(grid) * (dx **2)
+    grid = make_grid(mobility, constants)
+    area = np.sum(grid) * (dx ** 2)
     return area
 
 
-def estimate(mobility, N, alpha=ALPHA, range=RANGE, dx=DX, dt=DT, beta=BETA):
-    with set_constants(alpha=alpha, range=range, dx=dx, dt=dt, beta=beta):
+def estimate(mobility, N, alpha=ALPHA, range=RANGE, dt=DT, beta=BETA):
+    with set_constants(alpha=alpha, range=range, dt=dt, beta=beta):
         p = Pool()
         results = p.map(calculate, [mobility for _ in xrange(N)])
         p.close()
         p.join()
     return results
 
-def MSE(mobility, N, alpha=ALPHA, range=RANGE, dx=DX, dt=DT, beta=BETA):
-    estimation = estimate(mobility, N, alpha=alpha, range=range, dx=dx, dt=dt, beta=beta)
+
+def MSE(mobility, N, alpha=ALPHA, range=RANGE, dt=DT, beta=BETA):
+    estimation = estimate(
+        mobility, N, alpha=alpha, range=range, dt=dt, beta=beta)
     mse = np.mean((np.array(estimation) - mobility)**2)
     return mse